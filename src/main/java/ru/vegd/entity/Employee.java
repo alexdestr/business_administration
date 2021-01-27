71faf06c8e23d5f0203ef912e9fe185e8cc821cb
VincentVega <alexdestr@gmail.com>
2021-01-27 16:49:05 +0300

    -Added validation on entity. -Remade entity for lombok.

--- src/main/java/ru/vegd/entity/Employee.java
+++ src/main/java/ru/vegd/entity/Employee.java
@@ -1,116 +1,50 @@
 package ru.vegd.entity;
 
-import org.springframework.lang.NonNull;
+import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 import java.time.LocalDate;
-import java.util.Objects;
 
 @Entity
 @Table(name = "employee")
-public class Employee {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class Employee implements Serializable {
+
+    private static final long serialVersionUID = 1449233550861998884L;
+
+    @NotNull
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long employeeId;
 
-    @Column(name = "first_name", nullable = false, unique = true)
+    @NotEmpty
+    @Column(name = "first_name", nullable = false, unique = true, length = 30)
     private String firstName;
 
-    @Column(name = "last_name", nullable = false, unique = true)
+    @NotEmpty
+    @Column(name = "last_name", nullable = false, unique = true, length = 30)
     private String lastName;
 
+    @NotNull
     @Column(name = "department_id", nullable = false, unique = true)
     private Long departmentId;
 
+    @NotEmpty
     @Column(name = "job_title", nullable = false, unique = true)
     private String jobTitle;
 
+    @NotEmpty
     @Enumerated(EnumType.STRING)
     @Column(name = "gender", nullable = false, unique = true)
     private Gender gender;
 
     @Column(name = "date_of_birth", nullable = false, unique = true)
     private LocalDate dateOfBirth;
-
-    public Employee() {
-    }
-
-    public Employee(Long employeeId, String firstName, String lastName, Long departmentId, String jobTitle, Gender gender, LocalDate dateOfBirth) {
-        this.employeeId = employeeId;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.departmentId = departmentId;
-        this.jobTitle = jobTitle;
-        this.gender = gender;
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public Long getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(Long employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Long getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getJobTitle() {
-        return jobTitle;
-    }
-
-    public void setJobTitle(String jobTitle) {
-        this.jobTitle = jobTitle;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(Gender gender) {
-        this.gender = gender;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Employee employee = (Employee) o;
-        return employeeId.equals(employee.employeeId);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(employeeId);
-    }
 }
